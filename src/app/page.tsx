f83a9315785eaa2eef5780ef5f87de4c4a8802f3
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2024-06-14 18:43:18 +0800

    fix:icons size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-// Icons
-import { Discord } from "@/assets/icons/discord";
-import { Instagram } from "@/assets/icons/instagram";
-import { Tiktok } from "@/assets/icons/tiktok";
-import { Twitch } from "@/assets/icons/twitch";
-import { X } from "@/assets/icons/x";
-import { Youtube } from "@/assets/icons/youtube";
-
-import { LINK1 } from '@/constants/link'
+import { LINK1, LINK2 } from '@/constants/link'
 
 export default function Home() {
   return (
@@ -22,12 +14,11 @@ export default function Home() {
           Will you accompany me to be a star in your life? (★‿★)
         </p>
         <div className="flex gap-x-2 justify-center">
-          <Link href={''}><Discord color="white" fontSize={23} /></Link>
-          <Link href={''}><Instagram color="white" fontSize={23} /></Link>
-          <Link href={''}><Tiktok color="white" fontSize={23} /></Link>
-          <Link href={''}><Twitch color="white" fontSize={23} /></Link>
-          <Link href={''}><X color="white" fontSize={23} /></Link>
-          <Link href={''}><Youtube color="white" fontSize={23} /></Link>
+          {LINK2.map((res, key) => {
+            return (
+              <Link target="_blank" key={key} href={res.url}>{res.icons}</Link>
+            )
+          })}
         </div>
         <div className='pt-14 w-full text-center mx-auto text-black space-y-4'>
           {LINK1.map((res, key) => {
@@ -35,6 +26,7 @@ export default function Home() {
               <Link
                 key={key}
                 href={res.url}
+                target="_blank"
                 className="w-full bg-[#FFE599] py-4 rounded-full font-semibold hover:scale-105 duration-300 grid grid-cols-3 items-center"
               >
                 <p className="text-left pl-4">{res.icons}</p>
--- src/constants/link.tsx
+++ src/constants/link.tsx
@@ -8,26 +8,53 @@ export const LINK1 = [
   {
     name: "sociabuzz",
     url: "https://sociabuzz.com/hoshikoremi/tribe",
-    icons: <Fish />
+    icons: <Fish fontSize={30}/>
   },
   {
     name: "Youtube",
     url: "https://www.youtube.com/@HoshikoRemi?si=ESnCVbuZM7RQIUr2",
-    icons: <Youtube />
+    icons: <Youtube fontSize={30}/>
   },
   {
     name: "Twitch",
     url: "https://www.twitch.tv/hoshiko_remi",
-    icons: <Twitch />
+    icons: <Twitch fontSize={30}/>
   },
   {
     name: "Discord Server",
     url: "https://discord.com/invite/vrARWNkrcG",
-    icons: <Discord />
+    icons: <Discord fontSize={30}/>
   },
   {
     name: "Instagram",
     url: "https://www.instagram.com/hoshikoremi",
-    icons: <Instagram />
+    icons: <Instagram fontSize={30}/>
+  },
+]
+export const LINK2 = [
+  {
+    name: "sociabuzz",
+    url: "https://sociabuzz.com/hoshikoremi/tribe",
+    icons: <Fish fontSize={23} color="white"/>
+  },
+  {
+    name: "Youtube",
+    url: "https://www.youtube.com/@HoshikoRemi?si=ESnCVbuZM7RQIUr2",
+    icons: <Youtube fontSize={23} color="white"/>
+  },
+  {
+    name: "Twitch",
+    url: "https://www.twitch.tv/hoshiko_remi",
+    icons: <Twitch fontSize={23} color="white"/>
+  },
+  {
+    name: "Discord Server",
+    url: "https://discord.com/invite/vrARWNkrcG",
+    icons: <Discord fontSize={23} color="white"/>
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/hoshikoremi",
+    icons: <Instagram fontSize={23} color="white"/>
   },
 ]
